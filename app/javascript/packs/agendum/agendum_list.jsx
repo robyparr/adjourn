4239f41db17e7e9a8f8b1cdd81c521d2e284461a
Roby Parr <roby.parr@gmail.com>
2017-06-19 23:45:48 +0900

    Fix new agendum always being in a new row.

--- app/javascript/packs/agendum/agendum_list.jsx
+++ app/javascript/packs/agendum/agendum_list.jsx
@@ -3,31 +3,31 @@ import React from 'react';
 // Controls
 import Agendum from './agendum';
 
+// Utils
+import _ from 'lodash';
+
 /*
  * Create a list of Agendum.
  */
 const AgendumList = (props) => {
     return(
-        <div>
-            <div className="row">
-                {
-                    props.agenda.map((agendum, key) => {
-                        return(
-                            <div className="col m4" key={key}>
-                                <Agendum agendum={agendum}
-                                    meetingID={props.meetingID}
-                                    handleAgendumAddRemove={props.handleAgendumAddRemove} />
-                            </div>
-                        );
-                    })
-                }
-            </div>
-            <div className="row">
-                <div className="col m4">
-                    <Agendum 
-                        meetingID={props.meetingID}
-                        handleAgendumAddRemove={props.handleAgendumAddRemove} />
-                </div>
+        <div className="row">
+            {
+                props.agenda.map((agendum, key) => {
+                    return(
+                        <div className="col m4" key={key}>
+                            <Agendum agendum={agendum}
+                                meetingID={props.meetingID}
+                                handleAgendumAddRemove={props.handleAgendumAddRemove} />
+                        </div>
+                    );
+                })
+            }
+            {/* Add new Agendum item */}
+            <div className="col m4">
+                <Agendum
+                    meetingID={props.meetingID}
+                    handleAgendumAddRemove={props.handleAgendumAddRemove} />
             </div>
         </div>
     );
