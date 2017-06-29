834cb92ba8e9abd8bde60a82cc0778544f7b931d
Roby Parr <roby.parr@gmail.com>
2017-06-29 22:09:25 +0900

    Handle updating of existing notes.

--- app/javascript/packs/agendum_note/agendum_note.jsx
+++ app/javascript/packs/agendum_note/agendum_note.jsx
@@ -33,6 +33,22 @@ export default class AgendumNote extends Component {
         });
     }
 
+    /*
+     * Handle the updating of an existing note's contents.
+     */
+    updateNote = (prop) => {
+        var url = `/meetings/${this.props.meetingID}`
+            + `/agenda/${this.props.agendumID}`
+            + `/notes/${this.state.note.id}`;
+
+        axios.patch(url, {
+            authenticity_token: Utils.getAuthenticityToken(),
+            agendum_note: prop
+        }).then(response => {
+            this.setState({ note: response.data });
+        });
+    }
+
     render() {
         var isExisting = this.state.note.id;
 
@@ -42,7 +58,7 @@ export default class AgendumNote extends Component {
         return(
             <li className={noteClass}>
                 <RIETextArea
-                    change={this.addNewNote}
+                    change={isExisting ? this.updateNote : this.addNewNote}
                     value={isExisting ? this.state.note.content : 'Add a new note...'}
                     propName="content" />
             </li>
