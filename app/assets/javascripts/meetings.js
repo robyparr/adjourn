9dc6b680d66b8352835b5808e2c12ac0abd20c77
Roby Parr <roby.parr@gmail.com>
2017-07-02 21:18:21 +0900

    Fix dates sometimes not being localized on page load.

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -1,10 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
 
+var ready = function() {
     // localize times
     $('.localize').each(function () {
         var element = $(this);
         element.html(moment(element.html()).format('LLL'));
     });
-});
+};
+
+$(document).ready(ready);
+$(document).on('turbolinks:load', ready);
