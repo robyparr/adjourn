4c5060ffc289519d2cb611443e20d8838c7b4be1
Roby Parr <roby.parr@gmail.com>
2017-09-24 16:50:11 +0900

    Render markdown tables in meeting email.

--- app/helpers/export_helper.rb
+++ app/helpers/export_helper.rb
@@ -3,7 +3,7 @@ module ExportHelper
     text ||= ""
     
     renderer_options = { filter_html: true }
-    markdown_extensions = { autolink: true }
+    markdown_extensions = { autolink: true, tables: true }
 
     renderer = Redcarpet::Render::HTML.new(renderer_options)
     markdown = Redcarpet::Markdown.new(renderer, markdown_extensions)
