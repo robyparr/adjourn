d6ba56cb130930954b599da94b87090eedb8dd82
Roby Parr <roby.parr@gmail.com>
2017-10-22 18:34:07 +0900

    Move actionItem state modifications from actions to reducers.

--- app/javascript/actions/actionItems.js
+++ app/javascript/actions/actionItems.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 import Utils from 'utils';
 
 export const SET_ACTION_ITEMS = 'SET_ACTION_ITEMS';
+export const RECEIVE_NEW_ACTION_ITEM = 'RECEIVE_NEW_ACTION_ITEM';
+export const RECEIVE_UPDATED_ACTION_ITEM = 'RECEIVE_UPDATED_ACTION_ITEM';
+export const REMOVE_DELETED_ACTION_ITEM = 'REMOVE_DELETED_ACTION_ITEM';
 
+/**
+ * Set the entire state to the passed action items.
+ */
 export function setActionItems(actionItems) {
     return {
         type: SET_ACTION_ITEMS,
@@ -10,6 +16,9 @@ export function setActionItems(actionItems) {
     };
 }
 
+/**
+ * Adds a new action item on the backend.
+ */
 export function addActionItem(actionItem) {
     return function (dispatch, getState) {
         axios({
@@ -20,19 +29,27 @@ export function addActionItem(actionItem) {
                 action_item: actionItem
             }
         })
-        .then(response => {
-            const updatedActionItems = [
-                ...getState().actionItems,
-                response.data
-            ];
-            dispatch(setActionItems(updatedActionItems));
-        })
+        .then(response => dispatch(receiveNewActionItem(response.data)))
         .catch(error => console.log(error));
     }
 }
 
+/**
+ * Receives a newly created action item from the backend
+ * and adds it to the application state.
+ */
+export function receiveNewActionItem(actionItem) {
+    return {
+        type: RECEIVE_NEW_ACTION_ITEM,
+        actionItem
+    };
+}
+
+/**
+ * Updates the specified action item on the backend.
+ */
 export function updateActionItem(actionItemID, partialActionItem) {
-    return function (dispatch, getState) {
+    return function (dispatch) {
         axios({
             url: `/action_items/${actionItemID}`,
             method: 'PATCH',
@@ -41,22 +58,25 @@ export function updateActionItem(actionItemID, partialActionItem) {
                 action_item: partialActionItem
             }
         })
-        .then(response => {
-            const updatedActionItems = getState()
-                .actionItems
-                .map(item => {
-                    if (item.id === response.data.id) {
-                        return response.data;
-                    }
-
-                    return item;
-                });
-            dispatch(setActionItems(updatedActionItems));
-        })
+        .then(response => dispatch(receiveUpdatedActionItem(response.data)))
         .catch(error => console.log(error));
     }
 }
 
+/**
+ * Receives an updated action item from the backend and
+ * updates it in the application state.
+ */
+export function receiveUpdatedActionItem(actionItem) {
+    return {
+        type: RECEIVE_UPDATED_ACTION_ITEM,
+        actionItem
+    };
+}
+
+/**
+ * Deletes the action item on the backend.
+ */
 export function deleteActionItem(actionItemID) {
     return function (dispatch, getState) {
         axios({
@@ -64,11 +84,17 @@ export function deleteActionItem(actionItemID) {
             method: 'delete',
             data: { authenticity_token: Utils.getAuthenticityToken() }
         })
-        .then(response => {
-            const updatedActionItems = getState()
-                .actionItems
-                .filter(item => item.id !== actionItemID);
-            dispatch(setActionItems(updatedActionItems));
-        });
+        .then(response => dispatch(removeDeletedActionItem(actionItemID)))
+        .catch(error => console.log(error));
     }
+}
+
+/**
+ * Removes a deleted action item from the application state.
+ */
+export function removeDeletedActionItem(actionItemID) {
+    return {
+        type: REMOVE_DELETED_ACTION_ITEM,
+        actionItemID
+    };
 }
--- app/javascript/reducers/actionItems.js
+++ app/javascript/reducers/actionItems.js
@@ -1,4 +1,9 @@
-import { SET_ACTION_ITEMS } from '../actions/actionItems';
+import { 
+    SET_ACTION_ITEMS,
+    RECEIVE_NEW_ACTION_ITEM,
+    RECEIVE_UPDATED_ACTION_ITEM,
+    REMOVE_DELETED_ACTION_ITEM
+} from '../actions/actionItems';
 
 const initialState = [];
 
@@ -6,6 +11,23 @@ export default function actionItems(state = initialState, action) {
     switch(action.type) {
         case SET_ACTION_ITEMS:
             return action.actionItems;
+
+        case RECEIVE_NEW_ACTION_ITEM:
+            return [
+                ...state,
+                action.actionItem
+            ];
+
+        case RECEIVE_UPDATED_ACTION_ITEM:
+            return state.map(actionItem => {
+                if (actionItem.id === action.actionItem.id) {
+                    return action.actionItem;
+                }
+                return actionItem;
+            });
+
+        case REMOVE_DELETED_ACTION_ITEM:
+            return state.filter(actionItem => actionItem.id !== action.actionItemID);
 
         default:
             return state;
