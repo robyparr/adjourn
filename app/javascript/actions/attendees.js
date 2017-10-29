1becc7d8a5ce0c13689ed7d11519af9bb23962f6
Roby Parr <roby.parr@gmail.com>
2017-10-29 20:04:09 +0900

    Move attendees state modifications from actions to reducers.

--- app/javascript/actions/attendees.js
+++ app/javascript/actions/attendees.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 import Utils from 'utils';
 
 export const SET_ATTENDEES = 'SET_ATTENDEES';
+export const RECEIVE_NEW_ATTENDEE = 'RECEIVE_NEW_ATTENDEE';
+export const RECEIVE_REMOVED_ATTENDEE = 'RECEIVE_REMOVED_ATTENDEE';
 
+/**
+ * Set the state's attendees.
+ */
 export function setAttendees(attendees) {
     return {
         type: SET_ATTENDEES,
@@ -10,36 +15,60 @@ export function setAttendees(attendees) {
     };
 }
 
+/**
+ * Add an attendee to the meeting on the backend.
+ */
 export const addAttendee = (email, meetingID) => {
-    return function (dispatch, getState) {
+    return function (dispatch) {
         axios({
             method: 'POST',
             url: `/meetings/${meetingID}/attendees/attend`,
             data: { email: email, authenticity_token: Utils.getAuthenticityToken() }
         })
         .then(response => {
-            const updatedAttendees = [
-                ...getState().attendees,
-                {
-                    id: response.data.id,
-                    email: response.data.email
-                }
-            ];
-            dispatch(setAttendees(updatedAttendees));
+            const attendee = {
+                id: response.data.id,
+                email: response.data.email
+            };
+            dispatch(receiveNewAttendee(attendee));
         })
         .catch(error => console.log(error));
     }
 }
 
+/**
+ * Receives a newly added attendee from the backend
+ * and adds it to the frontend's state.
+ */
+export function receiveNewAttendee(attendee) {
+    return {
+        type: RECEIVE_NEW_ATTENDEE,
+        attendee
+    };
+}
+
+/**
+ * Removes an attendee from the meeting on the backend.
+ */
 export const removeAttendee = (email, meetingID) => {
-    return function (dispatch, getState) {
+    return function (dispatch) {
         axios({
             method: 'DELETE',
             url: `/meetings/${meetingID}/attendees`,
             data: { email: email, authenticity_token: Utils.getAuthenticityToken() }
-        }).then((response) => {
-            const updatedAttendees = getState().attendees.filter(a => a.email !== email);
-            dispatch(setAttendees(updatedAttendees));
-        });
+        })
+        .then(response => dispatch(receiveRemovedAttendee(email)))
+        .catch(error => console.log(error));
     }
+}
+
+/**
+ * Receives a removed attendee from the backend
+ * and removes it from the frontend's state.
+ */
+export function receiveRemovedAttendee(email) {
+    return {
+        type: RECEIVE_REMOVED_ATTENDEE,
+        email
+    };
 }
--- app/javascript/reducers/attendees.js
+++ app/javascript/reducers/attendees.js
@@ -1,4 +1,8 @@
-import { SET_ATTENDEES } from '../actions/attendees';
+import { 
+    SET_ATTENDEES,
+    RECEIVE_NEW_ATTENDEE,
+    RECEIVE_REMOVED_ATTENDEE
+} from '../actions/attendees';
 
 const initialState = [];
 
@@ -6,6 +10,12 @@ export default function attendees(state = initialState, action) {
     switch(action.type) {
         case SET_ATTENDEES:
             return action.attendees;
+
+        case RECEIVE_NEW_ATTENDEE:
+            return [ ...state, action.attendee ];
+
+        case RECEIVE_REMOVED_ATTENDEE:
+            return state.filter(attendee => attendee.email !== action.email);
 
         default:
             return state;
