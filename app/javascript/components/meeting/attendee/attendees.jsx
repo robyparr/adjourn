f7058bf22f7ed67898bfc43efe433ef63a5f50fc
Roby Parr <roby.parr@gmail.com>
2017-10-30 22:31:52 +0900

    Auto-focus attendees field after attendee added. This makes it easy to add multiple attendees as it will no longer be necessary to click the auto complete field each time you add a new attendee.

--- app/javascript/components/meeting/attendee/attendees.jsx
+++ app/javascript/components/meeting/attendee/attendees.jsx
@@ -8,41 +8,51 @@ import Avatar from 'material-ui/Avatar';
 // Utils
 import Utils from 'utils';
 
-const Attendees = (props) => {
-    return (
-        <div>
-            <h5>Attendees</h5>
+export default class Attendees extends React.Component {
 
-            <div className="print-hide">
-                <AutoComplete
-                    hintText="Add Attendee"
-                    className="browser-default"
-                    fullWidth={true}
-                    dataSource={props.attendeeResults}
-                    onUpdateInput={props.onInputUpdate}
-                    onNewRequest={(value, index) => {
-                        props.onAttendeeSelect(value, props.meetingID);
-                    }}
-                    searchText={props.searchText} />
-            </div>
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.attendees.length > prevProps.attendees.length) {
+            this.autoComplete.focus();
+        }
+    }
 
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                {props.attendees.map(attendee => {
-                    return (
-                        <Chip 
-                            key={attendee.id}
-                            style={{ margin: 4 }}
-                            onRequestDelete={() => {
-                                props.onAttendeeRemove(attendee.email, props.meetingID);
-                            }}>
-                            <Avatar src={Utils.getGravatarUrl(attendee.email)} />
-                            {attendee.email}
-                        </Chip>
-                    )
-                })}
+    onNewrequest = (value, index) => {
+        this.props.onAttendeeSelect(value, this.props.meetingID);
+    }
+
+    render() {
+        return (
+            <div>
+                <h5>Attendees</h5>
+
+                <div className="print-hide">
+                    <AutoComplete
+                        hintText="Add Attendee"
+                        className="browser-default"
+                        fullWidth={true}
+                        dataSource={this.props.attendeeResults}
+                        onUpdateInput={this.props.onInputUpdate}
+                        onNewRequest={this.onNewrequest}
+                        searchText={this.props.searchText}
+                        ref={input => this.autoComplete = input} />
+                </div>
+
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {this.props.attendees.map(attendee => {
+                        return (
+                            <Chip
+                                key={attendee.id}
+                                style={{ margin: 4 }}
+                                onRequestDelete={() => {
+                                    this.props.onAttendeeRemove(attendee.email, this.props.meetingID);
+                                }}>
+                                <Avatar src={Utils.getGravatarUrl(attendee.email)} />
+                                {attendee.email}
+                            </Chip>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
-
-export default Attendees;
