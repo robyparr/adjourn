0b6e8b778fcf8f65ab9460c476a5d4c09de7fc48
Roby Parr <roby.parr@gmail.com>
2018-01-29 22:52:21 +0900

    Add a simple seed for the dev test user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  email = "roby.parr@gmail.com".freeze
+  unless User.exists? email: email
+    User.create(
+      email: email,
+      password: "password",
+      password_confirmation: "password",
+      confirmed_at: Time.zone.now
+    )
+
+    puts "User '#{email}' created."
+  end
+end
