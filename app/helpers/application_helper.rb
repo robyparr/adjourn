5c3fd064aa228da94a17a81d23a0d592fc47a362
Roby Parr <roby.parr@gmail.com>
2018-02-11 23:53:21 +0900

    Add dynamic page titles.
    When working with multiple meetings in tabs, it's nice to see
    the meeting title in the browser tab. This allows the user to
    see the meeting title when in meetings or just "Adjourn" when
    on other pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def page_title(default_title)
+    # Idea for compacting array: https://stackoverflow.com/a/185980
+    [content_for(:title),default_title].compact.join(" | ")
+  end
 
   def gravatar_url(email, size = 64)
     hashed_email = Digest::MD5.hexdigest(email.strip.downcase)
