7aa51fc8d2b39e52679f9cff2c218b89c4807512
Roby Parr <roby.parr@gmail.com>
2018-05-06 12:16:03 +0900

    Refactor and optimize fetching of Google Calendar events.
    - Moved bulk of code into the user model and out of the controller.
    - Fixed n+1 query when checking if an event has already been added.
    - Fixed n+1 query when loading synced calendars of google accounts.

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,24 +1,7 @@
 class CalendarEventsController < ApplicationController
   def index
-    has_google_accounts = false
-    has_synced_calendars = false
-
-    events = current_user.google_accounts.map do |account|
-      has_google_accounts = true
-      account.google_calendars.map do |calendar|
-        has_synced_calendars = true
-        GoogleService::CalendarEvent.new(account, calendar.google_id).all
-      end
-    end
-      .flatten
-      .reject { |it| current_user.meetings.where(google_event_id: it[:id]).any? }
-      .sort_by { |it| it[:start] }
-
-    render partial: 'index.html', locals: {
-      has_google_accounts: has_google_accounts,
-      has_synced_calendars: has_synced_calendars,
-      events: events
-    }
+    calendar_events_hash = current_user.load_calendar_events
+    render partial: 'index.html', locals: calendar_events_hash
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,37 @@ class User < ApplicationRecord
   has_many :uploads, through: :agenda
   has_many :notes, class_name: 'AgendumNote', through: :meetings
   has_many :google_accounts, dependent: :destroy
+
+
+  def load_calendar_events
+    @has_google_accounts = false
+    @has_synced_calendars = false
+
+    events = fetch_google_calendar_events
+      .yield_self { |events| filter_out_added_calendar_events(events) }
+      .yield_self { |events| events.sort_by { |it| it[:start] } }
+
+    { has_google_accounts: @has_google_accounts,
+      has_synced_calendars: @has_synced_calendars,
+      events: events }
+  end
+
+  private
+
+  def fetch_google_calendar_events
+    google_accounts.includes(:google_calendars).map do |account|
+      @has_google_accounts = true
+      account.google_calendars.map do |calendar|
+        @has_synced_calendars = true
+        GoogleService::CalendarEvent.new(account, calendar.google_id).all
+      end
+    end.flatten
+  end
+
+  def filter_out_added_calendar_events(events)
+    return events unless events.present?
+    added_event_ids = meetings.where('google_event_id IS NOT NULL')
+      .pluck :google_event_id
+    events.reject { |it| added_event_ids.include? it[:id] }
+  end
 end
