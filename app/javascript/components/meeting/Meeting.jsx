1d9c7779372611534513fef2a521f3c4575a8c15
Roby Parr <roby.parr@gmail.com>
2018-12-07 18:37:25 +0900

    Cleanup meeting page

--- app/javascript/components/meeting/Meeting.jsx
+++ app/javascript/components/meeting/Meeting.jsx
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // Controls
-import DatePicker from 'material-ui/DatePicker';
-import TimePicker from 'rc-time-picker';
-
 import AgendaContainer from '../../containers/AgendaContainer';
 import ActionItemsContainer from '../../containers/ActionItemsContainer';
 import AttendeesContainer from '../../containers/AttendeesContainer';
 import AgendumDetailsContainer from '../../containers/AgendumDetailsContainer';
-import Attendees from './attendee/Attendees';
 import DateTimePicker from '../common/date_time_picker';
 import InlineEdit from '../common/InlineEdit';
 
 // Utilities
-import Utils from 'utils';
-import axios from 'axios';
 import moment from 'moment';
 
 /*
@@ -30,102 +24,93 @@ export default class Meeting extends React.Component {
     const endDate = moment(this.props.meeting.end_date).utc().local();
     return (
       <div>
-        {/* Title */}
-        <div className="row">
-          <div className="col m9">
-            <InlineEdit
-              name="title"
-              onChange={this.props.onFieldUpdate}
-              displayElement='h4'
-              value={this.props.meeting.title}
-              singleClickToEdit={!this.props.meeting.id} />
-          </div>
-        </div>
+        <div className="meeting-main-content">
+          {/* Title */}
+          <InlineEdit
+            name="title"
+            onChange={this.props.onFieldUpdate}
+            displayElement='h4'
+            value={this.props.meeting.title}
+            singleClickToEdit={!this.props.meeting.id} />
 
-        {/* Date & times */}
-        <div className="row">
-          <div className="col m4">
-            <div className="row">
-              <div className="col m1 bold">Start</div>
-              <div className="col m3">
-                <DateTimePicker
-                  name="start_date"
-                  dateTime={startDate}
-                  onChange={this.props.onFieldUpdate} />
+          {/* Date & times */}
+          <div className="row margin-bottom-none">
+            <div className="col m6">
+              <div className="row margin-bottom-none">
+                <div className="col m1 bold">Start</div>
+                <div className="col m3">
+                  <DateTimePicker
+                    name="start_date"
+                    dateTime={startDate}
+                    onChange={this.props.onFieldUpdate} />
+                </div>
+              </div>
+
+              <div className="row margin-bottom-none">
+                <div className="col m1 bold">End</div>
+                <div className="col m3">
+                  <DateTimePicker
+                    name="end_date"
+                    dateTime={endDate}
+                    onChange={this.props.onFieldUpdate} />
+                </div>
               </div>
             </div>
-
-            <div className="row">
-              <div className="col m1 bold">End</div>
-              <div className="col m3">
-                <DateTimePicker
-                  name="end_date"
-                  dateTime={endDate}
-                  onChange={this.props.onFieldUpdate} />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          {/* Action items, Attendees, & Agendum details */}
-          <div className="meeting-sidebar col m3 z-depth-3 card-panel">
-            <ul className="collapsible" data-collapsible="accordion">
-              <li>
-                <div className="collapsible-header active">
-                    <h5>Attendees</h5>
-                    <hr className="print-only" />
-                </div>
-                <div id="attendees" className="collapsible-body">
-                  <AttendeesContainer />
-                </div>
-              </li>
-              <li>
-                <div className="collapsible-header">
-                    <h5>Action Items</h5>
-                    <hr className="print-only" />
-                </div>
-                <div className="collapsible-body">
-                  <ActionItemsContainer />
-                </div>
-              </li>
-              <li id="agendum-details">
-                <div className="collapsible-header">
-                  <h5>Agendum Details</h5>
-                </div>
-                <div className="collapsible-body">
-                    <AgendumDetailsContainer />
-                  </div>
-              </li>
-            </ul>
           </div>
 
           {/* Agenda */}
-          <div className="col m9">
-            <div>
-              <h5>Agenda</h5>
-              <hr />
-              <AgendaContainer />
-            </div>
-          </div>
+          <h5 className="sub-header">Agenda</h5>
+          <AgendaContainer />
+        </div>
 
-          {/* FAB */}
-          <div className="fixed-action-btn click-to-toggle">
-            <a className="btn-floating btn-large red">
-              <i className="material-icons">menu</i>
-            </a>
-            <ul>
-              <li>
-                <a className="btn-floating red tooltipped"
-                  data-position="left"
-                  data-delay="50"
-                  onClick={this.props.onEmailAttendeesClick}
-                  data-tooltip="Email Attendees">
-                  <i className="material-icons">send</i>
-                </a>
-              </li>
-            </ul>
-          </div>
+        {/* Action items, Attendees, & Agendum details */}
+        <div className="meeting-sidebar col m3 z-depth-3 card-panel">
+          <ul className="collapsible" data-collapsible="accordion">
+            <li>
+              <div className="collapsible-header active">
+                  <h5>Attendees</h5>
+                  <hr className="print-only" />
+              </div>
+              <div id="attendees" className="collapsible-body">
+                <AttendeesContainer />
+              </div>
+            </li>
+            <li>
+              <div className="collapsible-header">
+                  <h5>Action Items</h5>
+                  <hr className="print-only" />
+              </div>
+              <div className="collapsible-body">
+                <ActionItemsContainer />
+              </div>
+            </li>
+            <li id="agendum-details">
+              <div className="collapsible-header">
+                <h5>Agendum Details</h5>
+              </div>
+              <div className="collapsible-body">
+                  <AgendumDetailsContainer />
+                </div>
+            </li>
+          </ul>
+        </div>
+
+        {/* FAB */}
+        <div className="fixed-action-btn click-to-toggle">
+          <a className="btn-floating btn-large red">
+            <i className="material-icons">menu</i>
+          </a>
+          <ul>
+            <li>
+              <a className="btn-floating red tooltipped"
+                data-position="left"
+                data-delay="50"
+                onClick={this.props.onEmailAttendeesClick}
+                data-tooltip="Email Attendees">
+                <i className="material-icons">send</i>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     );
