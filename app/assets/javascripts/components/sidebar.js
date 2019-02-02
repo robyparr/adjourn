8f771ea4ed77e80a4a4a3da5f98494a895557345
Roby Parr <roby.parr@gmail.com>
2019-02-03 03:13:16 +0900

    Mobile optimizations for the sidebar and meeting index page

--- app/assets/javascripts/components/sidebar.js
+++ app/assets/javascripts/components/sidebar.js
@@ -2,20 +2,35 @@ $(document).ready(function() {
   Sidebar = (function() {
     var mainContent = $('.content');
     var sidebar = $('.sidebar');
-  
+    var sidebarOverlay = $('.sidebar-overlay');
+
     return {
       isCollapsed: function() {
         return sidebar.hasClass('collapsed');
       },
-  
+
       collapse: function() {
         sidebar.addClass('collapsed');
         mainContent.addClass('with-collapsed-sidebar');
       },
-  
+
       expand: function() {
         sidebar.removeClass('collapsed');
         mainContent.removeClass('with-collapsed-sidebar');
+      },
+
+      show: function() {
+        sidebar.removeClass('sidebar-hidden');
+        sidebar.css('margin-left', '0');
+        sidebarOverlay.css('display', 'block');
+      },
+
+      hide: function() {
+        setTimeout(function() {
+          sidebar.addClass('sidebar-hidden');
+        }, 500);
+        sidebar.css('margin-left', '-80%');
+        sidebarOverlay.css('display', 'none');
       }
     };
   })();
@@ -26,5 +41,14 @@ $(document).ready(function() {
     } else {
       Sidebar.collapse();
     }
+  });
+
+  $(document).on('click', '#sidebar-menu', function(e) {
+    Sidebar.expand();
+    Sidebar.show();
+  });
+
+  $(document).on('click', '.sidebar-overlay', function(e) {
+    Sidebar.hide();
   });
 });
