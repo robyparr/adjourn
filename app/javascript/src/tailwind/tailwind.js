2903d292cb42e904f17c9176cc662c9cedcfa54f
Roby Parr <roby.parr@gmail.com>
2019-03-16 13:23:28 +0900

    Replace MaterializeCSS pagination styles

--- app/javascript/src/tailwind/tailwind.js
+++ app/javascript/src/tailwind/tailwind.js
@@ -47,6 +47,8 @@ let colors = {
   'primary': '#2196f3',
   'primary-dark': '#0b7dda',
   'primary-light': '#3da2f5',
+  'primary-lighter': '#59ACF0',
+  'primary-lightest': '#7EBBEE',
 
   'transparent': 'transparent',
 
