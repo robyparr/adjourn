f2d40f861cb9edbdf4b9703d924f089606dbce50
Roby Parr <roby.parr@gmail.com>
2019-03-26 22:16:30 +0900

    Potential fix for production email links containing port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,10 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = {
-    host: ENV['HOST'],
-    port: ENV['PORT']
-  }
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
