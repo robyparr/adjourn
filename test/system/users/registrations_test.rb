fcaabb4a2075fdc03cf9ce0c3bcac87791d80bd6
Roby Parr <roby.parr@gmail.com>
2019-06-02 13:41:10 +0900

    Add signup system test

---
+++ test/system/users/registrations_test.rb
@@ -0,0 +1,20 @@
+require "application_system_test_case"
+
+module Users
+  class RegistrationsTest < ApplicationSystemTestCase
+    test 'signup' do
+      visit new_user_registration_url
+      assert_selector ".card-title", text: "Sign Up"
+
+      assert_difference 'User.count', 1 do
+        fill_in 'Email', with: 'new_user@example.com'
+        fill_in 'Password', with: 'password123'
+        fill_in 'Password Confirmation', with: 'password123'
+        click_button 'Sign up'
+      end
+
+      new_user = User.last
+      assert_equal 'new_user@example.com', new_user.email
+    end
+  end
+end
