c5963b1fe4059993467e019f76195cc68e7e356b
Roby Parr <roby.parr@gmail.com>
2020-05-09 20:33:48 +0900

    Cleanup un-used dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery 
+//= require jquery
 //= require jquery_ujs
-//= require moment/moment
+//= require dayjs/dayjs.min
 //= require_tree .
--- app/javascript/utils.js
+++ app/javascript/utils.js
@@ -5,27 +5,6 @@ var md5 = require('md5');
 */
 export default class Utils {
   /*
-  * Formats a Date() object into a string.
-  */
-  static formatDateTime(date) {
-    return moment(date).format('LLLL');
-  }
-
-  /*
-  * Returns a Date() object from a string in the same format
-  * as formatDateTime();
-  */
-  static dateFromString(dateTimeString) {
-    if (typeof dateTimeString === 'object') {
-      // Assume that dateTimeString is a Date() object
-      // and reurn that.
-      return dateTimeString;
-    }
-
-    return new Date(dateTimeString.replace(' ', 'T').replace('Z', ''));
-  }
-
-  /*
   * Get the Rails authenticity token.
   */
   static getAuthenticityToken() {
@@ -37,18 +16,5 @@ export default class Utils {
     const hashedEmail = md5(email.trim().toLowerCase());
     const size = options.size || 32
     return `https://www.gravatar.com/avatar/${hashedEmail}.jpg?s=${size}&d=mm`;
-  }
-
-  // Based on https://stackoverflow.com/a/2234986
-  static isDescendantOf(parent, child) {
-    let childParent = child.parentNode;
-
-    while (childParent != null) {
-      if (childParent == parent) return true;
-
-      childParent = childParent.parentNode;
-    }
-
-    return false;
   }
 }
